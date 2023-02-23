62f2b632ff39db32a145697018cf481df46e948a
Papa Smurf <chris@violet.co>
2023-02-23 10:48:55 +0000

    feat: @uniswap/v3-core -> @violetprotocol/mauve-v3-core

--- contracts/SwapRouter.sol
+++ contracts/SwapRouter.sol
@@ -2,9 +2,9 @@
 pragma solidity =0.7.6;
 pragma abicoder v2;
 
-import '@uniswap/v3-core/contracts/libraries/SafeCast.sol';
-import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
-import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import '@violetprotocol/mauve-v3-core/contracts/libraries/SafeCast.sol';
+import '@violetprotocol/mauve-v3-core/contracts/libraries/TickMath.sol';
+import '@violetprotocol/mauve-v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 
 import './interfaces/ISwapRouter.sol';
 import './base/PeripheryImmutableState.sol';
