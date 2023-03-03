01b37eb404cbbace8b55b972ec39e5a48cd8f824
Papa Smurf <chris@violet.co>
2023-03-03 17:45:47 +0000

    feat: added EAT compatibility to Multicall

---
+++ contracts/base/EATMulticall.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import '@violetprotocol/ethereum-access-token/contracts/AccessTokenConsumer.sol';
+import '../interfaces/IEATMulticall.sol';
+import './Multicall.sol';
+
+/// @title Ethereum Access Token Multicall
+/// @notice Enables calling multiple methods in a single call to the contract
+abstract contract EATMulticall is Multicall, IEATMulticall, AccessTokenConsumer {
+    function multicall(
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        uint256 expiry,
+        bytes[] calldata data
+    ) public payable override requiresAuth(v, r, s, expiry) returns (bytes[] memory results) {
+        // performs an external call to self for core multicall logic
+        this.multicall(data);
+    }
+
+    /// @inheritdoc IMulticall
+    function multicall(bytes[] calldata data) public payable override returns (bytes[] memory results) {
+        require(msg.sender == address(this));
+        super.multicall(data);
+    }
+}
