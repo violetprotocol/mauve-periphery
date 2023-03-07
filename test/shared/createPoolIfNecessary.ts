8e0b80b2c098a5f5c6d11d8beceb5307659f3a09
Lint Action <lint-action@samuelmeuli.com>
2023-03-07 17:56:51 +0000

    Fix code style issues with Prettier

--- test/shared/createPoolIfNecessary.ts
+++ test/shared/createPoolIfNecessary.ts
@@ -10,38 +10,41 @@ export type CreatePoolIfNecessary = (
   value?: { value: number }
 ) => Promise<string>
 
-export const createPoolIfNecessary =
-  (factory: IUniswapV3Factory, wallet: Wallet) =>
-  async (token0: string, token1: string, fee: FeeAmount, initialSqrtPriceX96: BigNumber) => {
-    const pool = await factory.getPool(token0, token1, fee)
+export const createPoolIfNecessary = (factory: IUniswapV3Factory, wallet: Wallet) => async (
+  token0: string,
+  token1: string,
+  fee: FeeAmount,
+  initialSqrtPriceX96: BigNumber
+) => {
+  const pool = await factory.getPool(token0, token1, fee)
 
-    if (pool == constants.AddressZero) {
-      try {
-        const createdPoolTx = await factory.createPool(token0, token1, fee)
-        const txReceipt = await createdPoolTx.wait()
-        const poolAddress = txReceipt.events?.[0].args?.pool
+  if (pool == constants.AddressZero) {
+    try {
+      const createdPoolTx = await factory.createPool(token0, token1, fee)
+      const txReceipt = await createdPoolTx.wait()
+      const poolAddress = txReceipt.events?.[0].args?.pool
 
-        if (!poolAddress) {
-          throw new Error('Failed to get pool address from creation')
-        }
+      if (!poolAddress) {
+        throw new Error('Failed to get pool address from creation')
+      }
 
-        const poolContract = await IUniswapV3Pool__factory.connect(poolAddress, wallet)
+      const poolContract = await IUniswapV3Pool__factory.connect(poolAddress, wallet)
+      await poolContract.initialize(initialSqrtPriceX96)
+
+      return poolAddress
+    } catch (error) {
+      throw new Error(`Failed to create new pool: ${error}`)
+    }
+  } else {
+    try {
+      const poolContract = await IUniswapV3Pool__factory.connect(pool, wallet)
+      const { sqrtPriceX96 } = await poolContract.slot0()
+      if (sqrtPriceX96.eq(0)) {
         await poolContract.initialize(initialSqrtPriceX96)
-
-        return poolAddress
-      } catch (error) {
-        throw new Error(`Failed to create new pool: ${error}`)
       }
-    } else {
-      try {
-        const poolContract = await IUniswapV3Pool__factory.connect(pool, wallet)
-        const { sqrtPriceX96 } = await poolContract.slot0()
-        if (sqrtPriceX96.eq(0)) {
-          await poolContract.initialize(initialSqrtPriceX96)
-        }
-        return poolContract.address
-      } catch (error) {
-        throw new Error(`Failed to handle already deployed pool: ${error}`)
-      }
+      return poolContract.address
+    } catch (error) {
+      throw new Error(`Failed to handle already deployed pool: ${error}`)
     }
   }
+}
