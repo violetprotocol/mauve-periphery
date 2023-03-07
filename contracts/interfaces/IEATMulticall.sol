170f5d3b28f52f95347f0be6b10a75d070ba6076
Papa Smurf <chris@violet.co>
2023-03-07 11:36:03 +0000

    chore: comment updated in IEATMulticall

--- contracts/interfaces/IEATMulticall.sol
+++ contracts/interfaces/IEATMulticall.sol
@@ -7,10 +7,10 @@ pragma abicoder v2;
 interface IEATMulticall {
     /// @notice Call multiple functions in the current contract and return the data from all of them if they all succeed
     /// @dev The `msg.value` should not be trusted for any method callable from multicall.
-    /// @param v The encoded function data for each of the calls to make to this contract
-    /// @param r The encoded function data for each of the calls to make to this contract
-    /// @param s The encoded function data for each of the calls to make to this contract
-    /// @param expiry The encoded function data for each of the calls to make to this contract
+    /// @param v The v component of the signed EAT signature
+    /// @param r The r component of the signed EAT signature
+    /// @param s The s component of the signed EAT signature
+    /// @param expiry The expiry of the signed EAT
     /// @param data The encoded function data for each of the calls to make to this contract
     /// @return results The results from each of the calls passed in via data
     function multicall(
