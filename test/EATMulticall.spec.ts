1cf757f00d20d0e889f980cc441f7a1ec585f2e2
Papa Smurf <chris@violet.co>
2023-03-07 13:11:48 +0000

    test: fixed all gas tests

--- test/EATMulticall.spec.ts
+++ test/EATMulticall.spec.ts
@@ -12,7 +12,7 @@ import { parseEther, splitSignature } from 'ethers/lib/utils'
 // DO NOT USE IN SENSITIVE PLACES
 const EAT_ISSUER_PK = '18eaafaa63636879094c86a953e6fcba4abaefae3baec1d4e5b952c10828d4c2'
 
-describe('Multicall', async () => {
+describe('EATMulticall', async () => {
   let signer: Wallet
   let wallets: Wallet[]
 
