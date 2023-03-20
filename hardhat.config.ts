ceb5eb473d3fa56053e46dc95f7f8b249f2be529
Papa Smurf <chris@violet.co>
2023-03-20 10:42:17 +0000

    fix: revert changes to disallow unlimited contract size

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -50,7 +50,7 @@ const DEFAULT_COMPILER_SETTINGS = {
 export default {
   networks: {
     hardhat: {
-      allowUnlimitedContractSize: true,
+      allowUnlimitedContractSize: false,
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
