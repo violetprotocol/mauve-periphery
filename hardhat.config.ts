78c94eff271691642b0c4dc18edbf5c7c6b00085
Gabriel Ferraz <sudoferraz@gmail.com>
2023-03-23 14:49:47 +0200

    Bumps NonfungiblePositionManager optimizer runs to 850

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,7 @@ const LOW_OPTIMIZER_COMPILER_SETTINGS = {
     evmVersion: 'istanbul',
     optimizer: {
       enabled: true,
-      runs: 550,
+      runs: 850,
     },
     metadata: {
       bytecodeHash: 'none',
