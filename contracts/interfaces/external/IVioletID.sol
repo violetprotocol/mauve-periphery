d95e055d563fd3c8406afb60b974afbe4ea28e6e
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-05-05 15:26:16 +0100

    update violetID to 1.2.3, create mock VioletID contract

--- contracts/interfaces/external/IVioletID.sol
+++ contracts/interfaces/external/IVioletID.sol
@@ -2,6 +2,7 @@
 
 /// @title VioletID interface
 /// @notice Allows checking for VioletID registration status to allow or disallow accounts
+/// @dev Make sure to keep this in sync with the interface from @violetprotocol/violetid!
 pragma solidity >=0.7.5;
 
 interface IVioletID {
